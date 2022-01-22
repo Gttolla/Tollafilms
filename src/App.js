3bb52e05ec550a5b9c34993c09d39c7b3baec6a0
Gttolla <lucaspedrosupergeeks@gmail.com>
2022-01-22 04:44:20 -0300

    MovieRow no effect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import "./App.css"
+import MovieRow from "./components/MovieRow";
 
 export default() =>{
+
+  const [movieList, setMovieList] = useState([]);
 
     useEffect(()=>{
       const loadAll = async () => {
         let list = await Tmdb.getHomeList();
-        console.log(list);
+        setMovieList(list);
       }
 
       loadAll();
     }, []);
 
   return(
-    <h1>
-      Tollafilms!
-    </h1>
+    <div className="page">
+      <header>
+
+      </header>
+
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+      <footer>
+
+      </footer>
+    </div>
   );
 } 
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -13,42 +13,42 @@ export default {
             {
                 slug: 'originals',
                 title: 'Originais do Netflix',
-                item: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'trendings',
                 title: 'Recomendados para você',
-                item: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'toprated',
                 title: 'Em alta',
-                item: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                item: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                item: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                item: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                item: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Domentários',
-                item: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
         ]
     }
