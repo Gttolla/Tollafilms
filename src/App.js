c1312ce3e61fd8c07e2660378cfa6acbea593371
Gttolla <lucaspedrosupergeeks@gmail.com>
2022-01-22 22:44:35 -0300

    MovieRow, randomChosen

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
 import "./App.css"
 import MovieRow from "./components/MovieRow";
+import FeatureMovie from "./components/FeatureMovie";
 
 export default() =>{
 
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
 
     useEffect(()=>{
       const loadAll = async () => {
         let list = await Tmdb.getHomeList();
         setMovieList(list);
+
+        let originals = list.filter(i=>i.slug === 'originals'); 
+        let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+        let chosen = originals[0].items.results[randomChosen];
+
+        console.log(chosen);
       }
 
       loadAll();
@@ -21,6 +29,10 @@ export default() =>{
       <header>
 
       </header>
+
+      {featureData&& 
+        <FeatureMovie item={featureData}/>
+      }
 
       <section className="lists">
         {movieList.map((item, key)=>(
---
+++ src/components/FeatureMovie.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './FeatureMovie.css'
+
+export default () => {
+    return(
+        <div>FILME EM DESTAQUE</div>
+    )
+}
